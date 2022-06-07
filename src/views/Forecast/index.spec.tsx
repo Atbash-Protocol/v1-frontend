f2b1148853ec410f9d27f7c9d3191756638551a4
0xTex <tdza@mail.co>
2022-06-07 22:24:00 +0200

    feat: init forecast

--- src/views/Forecast/index.spec.tsx
+++ src/views/Forecast/index.spec.tsx
@@ -2,6 +2,8 @@ import { render } from '@testing-library/react';
 
 import Forecast from '.';
 
+jest.mock('./components/ForecastMetrics', () => () => <>Forecast Metrics</>);
+
 describe('Forecast', () => {
     it('renders', () => {
         const comp = render(<Forecast />);
--- src/views/Forecast/index.tsx
+++ src/views/Forecast/index.tsx
@@ -2,15 +2,23 @@ import { Typography } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 import { BCard } from 'components/BCard';
+import { theme } from 'constants/theme';
+
+import ForecastMetrics from './components/ForecastMetrics';
 
 function Forecast() {
     const { t } = useTranslation();
 
     return (
-        <BCard title={t('bond:Forecast')} zoom={true}>
-            <Typography>
-                <>{t('ComingSoon')} </>
+        <BCard title={t('globe:ForecastTitle')} zoom={true}>
+            <Typography sx={{ color: theme.palette.primary.light }}>
+                <>{t('globe:EstimateYourReturns')} </>
             </Typography>
+            <Typography sx={{ color: theme.palette.primary.light, mt: theme.spacing(2) }}>
+                <>{t('globe:ForecastWarning')} </>
+            </Typography>
+
+            <ForecastMetrics />
         </BCard>
     );
 }
