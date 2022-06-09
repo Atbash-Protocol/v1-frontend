ac0f801b302fec18f21a6fab3350d5f58a863d8d
0xTex <tlr.nodejs@yahoo.com>
2022-06-09 23:20:00 +0200

    feat: add core forecast

--- src/store/modules/app/app.selectors.ts
+++ src/store/modules/app/app.selectors.ts
@@ -7,6 +7,7 @@ import { formatUSD } from 'helpers/price-units';
 import { IReduxState } from 'store/slices/state.interface';
 import { RootState } from 'store/store';
 
+import { selectDaiPrice } from '../markets/markets.selectors';
 import { calculateStakingRewards } from './app.helpers';
 
 export const selectFormattedReservePrice = (state: RootState): string | null => {
--- src/views/Forecast/components/Form/index.tsx
+++ src/views/Forecast/components/Form/index.tsx
@@ -1,106 +1,177 @@
 import { useState } from 'react';
 
-import { Box, Grid, Slider, Typography } from '@mui/material';
+import { Box, Grid, Slider, styled, Typography } from '@mui/material';
+import { blue } from '@mui/material/colors';
+import Decimal from 'decimal.js';
 import { useTranslation } from 'react-i18next';
 
 import BInput from 'components/BInput';
 import { theme } from 'constants/theme';
+import { formatUSD } from 'helpers/price-units';
 
+import DaysPicker from '../DaysPicker';
+
+const MIN_DAYS = 1;
 const MAX_DAYS = 365;
+const LAMBO_PRICE = 222_004;
+const EPOCH_PER_DAY = 3;
 
-const DaysPicker = () => {
+enum ConfigurationItemEnum {
+    StakedSBAmount = 'StakedSBAmount',
+    RewardYieldPercent = 'RewardYieldPercent',
+    BASHPriceAtPurchase = 'BASHPriceAtPurchase',
+    FutureBASHMarketPrice = 'FutureBASHMarketPrice',
+}
+interface ConfigurationData {
+    StakedSBAmount: string;
+    RewardYieldPercent: string;
+    BASHPriceAtPurchase: string;
+    FutureBASHMarketPrice: string;
+}
+interface ConfigurationProps {
+    onConfigurationChange: (formValues: Record<ConfigurationItemEnum, string>) => void;
+    initialData: ConfigurationData;
+}
+
+const Configuration = ({ onConfigurationChange, initialData }: ConfigurationProps) => {
     const { t } = useTranslation();
-    const [days, setDays] = useState(0);
 
-    const handleCommitedChange = (event: React.SyntheticEvent | Event, sliderValue: number | Array<number>) => {
-        event.preventDefault();
-
-        console.log('Change', sliderValue);
-    };
-
-    const handleChange = (event: Event, sliderValue: number | Array<number>) => {
-        event.preventDefault();
-
-        const value = Array.isArray(sliderValue) ? sliderValue[0] : sliderValue;
-        setDays(value);
+    const handleConfigurationChange = (data: Record<ConfigurationItemEnum, string>) => {
+        console.log('Configuration', data, { ...initialData, ...data });
+        onConfigurationChange({ ...initialData, ...data });
     };
 
     return (
-        <Box width={100} height={'10rem'} sx={{ justifyContent: 'center', alignItems: 'center' }} pb={theme.spacing(2)}>
-            <Typography variant="body1" color={theme.palette.primary.light} mb={theme.spacing(2)}>
-                {t('common:day_other', { count: days })}
-            </Typography>
-            <Slider
-                orientation="vertical"
-                defaultValue={20}
-                value={days}
-                max={MAX_DAYS}
-                valueLabelDisplay="auto"
-                onChange={handleChange}
-                onChangeCommitted={handleCommitedChange}
-            />
-        </Box>
-    );
-};
-
-const Configuration = () => {
-    // get SBASH Amount, reward yield, BASH price from props
-
-    const [BASHAmount, SetBASHAmount] = useState('0');
-    const [rewardYield, setRewardYield] = useState('0');
-    const [bashPrice, setBashPrice] = useState('0');
-    const [futureBashPrice, setFutureBashPrice] = useState('0');
-
-    return (
-        <>
-            <Box>
-                <Typography variant="body1">sBASH AMOUNT</Typography>
-                <BInput onChange={() => SetBASHAmount('10')} defaultValue={'0'} maxValue={'300'} />
-            </Box>
-
-            <Box>
-                <Typography variant="body1">REWARD YIELD (%)</Typography>
-                <BInput onChange={() => setRewardYield('10')} defaultValue={'0'} maxValue={'300'} />
+        <Box color={theme.palette.primary.light}>
+            <Box color={theme.palette.primary.light}>
+                <Typography variant="body1" align="center">
+                    {t('globe:StakedSBAmount')}
+                </Typography>
+                <BInput name="StakedSBAmount" onChange={handleConfigurationChange} defaultValue={initialData.StakedSBAmount} placeholder={t('globe:EnterStakedBASHAmount')} />
             </Box>
 
             <Box>
                 <Typography variant="body1" align="center">
-                    BASH Price AT PURCHASE ($)
+                    {t('globe:RewardYieldPercent')}
                 </Typography>
-                <BInput onChange={() => setBashPrice('10')} defaultValue={'0'} maxValue={'300'} />
+                <BInput
+                    name="RewardYieldPercent"
+                    onChange={handleConfigurationChange}
+                    defaultValue={initialData.RewardYieldPercent}
+                    endAdornmentLabel={t('globe:Current')}
+                    placeholder={t('globe:EnterRewardYieldPercent')}
+                />
             </Box>
 
             <Box>
-                <Typography variant="body1">Future BASH MARKET PRICE ($)</Typography>
-                <BInput onChange={() => setFutureBashPrice('10')} defaultValue={'0'} maxValue={'300'} />
+                <Typography variant="body1" align="center">
+                    {t('globe:BASHPriceAtPurchase')}
+                </Typography>
+                <BInput
+                    name="BASHPriceAtPurchase"
+                    onChange={handleConfigurationChange}
+                    defaultValue={initialData.BASHPriceAtPurchase}
+                    endAdornmentLabel={t('globe:Current')}
+                    placeholder={t('globe:EnterBuyPrice')}
+                />
             </Box>
-        </>
+
+            <Box>
+                <Typography variant="body1" align="center">
+                    {t('globe:FutureBASHMarketPrice')}
+                </Typography>
+                <BInput
+                    name="FutureBASHMarketPrice"
+                    onChange={handleConfigurationChange}
+                    defaultValue={initialData.FutureBASHMarketPrice}
+                    endAdornmentLabel={t('globe:Current')}
+                    placeholder={t('globe:EnterFuturePrice')}
+                />
+            </Box>
+        </Box>
     );
 };
 
-const ForecastDetails = () => {
-    return (
-        <>
-            <span>Initial investment</span>
-            <span>Current wealth</span>
-            <span>Bash rewards estimation</span>
-            <span>Potential return</span>
-            <span>Lambo prices</span>
-        </>
+interface ForecastDetailsProps {
+    data: Record<ConfigurationItemEnum, string>;
+    initialPrice: Decimal;
+    duration: number;
+}
+
+const ForecastDetails = ({ data, duration, initialPrice }: ForecastDetailsProps) => {
+    const { t } = useTranslation();
+    const { StakedSBAmount, BASHPriceAtPurchase, RewardYieldPercent, FutureBASHMarketPrice } = data;
+
+    const dailyROI = (1 + Number(RewardYieldPercent) / 100) ** (duration * EPOCH_PER_DAY) - 1;
+    const BASHRewardsEstimation = Number(StakedSBAmount) * dailyROI;
+    const PotentialReturn = (Number(StakedSBAmount) + BASHRewardsEstimation) * Number(FutureBASHMarketPrice);
+    const nbLambo = Math.round(PotentialReturn / LAMBO_PRICE);
+
+    const computedForecast = Object.entries({
+        YourInitialInvestment: Number(StakedSBAmount) * Number(BASHPriceAtPurchase),
+        CurrentWealth: initialPrice.toNumber() * Number(StakedSBAmount),
+        PotentialReturn,
+    }).map(([key, value], index) => {
+        return (
+            <Box key={index} sx={{ justifyContent: 'space-between', color: 'white', display: 'inline-flex', width: '100%' }}>
+                <Typography variant="body1">{t(`globe:${key}`)}</Typography>
+                <Typography variant="body1" noWrap>
+                    {formatUSD(value, 2)}
+                </Typography>
+            </Box>
+        );
+    });
+
+    const bashEstimation = (
+        <Box key="bashEstimation" sx={{ justifyContent: 'space-between', color: 'white', display: 'inline-flex', width: '100%' }}>
+            <Typography variant="body1">{t('globe:BASHRewardsEstimation')}</Typography>
+            <Typography variant="body1">{Intl.NumberFormat('en', { notation: 'compact', maximumFractionDigits: 2 }).format(BASHRewardsEstimation)}</Typography>
+        </Box>
     );
+
+    const lambos = (
+        <Box key="lambos" sx={{ justifyContent: 'space-between', color: 'white', display: 'inline-flex', width: '100%' }}>
+            <Typography variant="body1">{t('globe:PotentialNumberLambo')}</Typography>
+            <Typography variant="body1">{nbLambo}</Typography>
+        </Box>
+    );
+
+    computedForecast.splice(2, 0, bashEstimation);
+
+    return <>{[...computedForecast, lambos]}</>;
 };
 
-const Form = () => {
+interface ForecastFormProps {
+    BASHPrice: Decimal;
+    SBASHBalance: Decimal;
+    stakingPercentage: Decimal;
+}
+const Form = ({ BASHPrice, stakingPercentage, SBASHBalance }: ForecastFormProps) => {
+    const initialData = {
+        BASHPrice,
+        SBASHBalance,
+        stakingPercentage,
+    };
+
+    const [formData, setFormData] = useState({
+        StakedSBAmount: initialData.SBASHBalance.toString(),
+        RewardYieldPercent: initialData.stakingPercentage.toFixed(2),
+        BASHPriceAtPurchase: initialData.BASHPrice.toFixed(2),
+        FutureBASHMarketPrice: initialData.BASHPrice.toFixed(2),
+    });
+
+    const [days, setDays] = useState(MIN_DAYS);
+
     return (
         <Grid container>
             <Grid item xs={5}>
-                <Configuration />
+                <Configuration onConfigurationChange={setFormData} initialData={formData} />
             </Grid>
             <Grid item xs={2}>
-                <DaysPicker />
+                <DaysPicker currentDay={days} onChange={setDays} minDays={MIN_DAYS} maxDays={MAX_DAYS} />
             </Grid>
             <Grid item xs={5}>
-                <ForecastDetails />
+                <ForecastDetails data={formData} initialPrice={BASHPrice} duration={days} />
             </Grid>
         </Grid>
     );
