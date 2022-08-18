dca96da652085ca5187652d7194b155dc115040b
0xTex <tlr.nodejs@yahoo.com>
2022-08-18 22:55:54 +0200

    chore: fix main build

--- src/components/BondLogo.tsx
+++ src/components/BondLogo.tsx
@@ -1,19 +1,19 @@
-import { Box } from "@material-ui/core";
-import { Bond } from "../helpers/bond/bond";
+import { Box } from '@material-ui/core';
+import { Bond } from '../helpers/bond/bond';
 
 interface IBondLogoProps {
     bond: Bond;
 }
 
 function BondLogo({ bond }: IBondLogoProps) {
-    let style = { height: "32px", width: "32px" };
+    let style = { height: '32px', width: '32px' };
 
     if (bond.isLP) {
-        style = { height: "32px", width: "62px" };
+        style = { height: '32px', width: '62px' };
     }
 
     return (
-        <Box display="flex" alignItems="center" justifyContent="center" width={"64px"}>
+        <Box display="flex" alignItems="center" justifyContent="center" width={'64px'}>
             <img src={bond.bondIconSvg} style={style} />
         </Box>
     );
